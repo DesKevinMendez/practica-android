6d4496dfcefd6c7b412017e3ac828d7faefd9483
Kevin Mendez <kevin_mendez30@outlook.com>
2019-09-18 15:59:59 -0600

    Se muestra informacion de usuario de forma dinamica

--- app/src/main/java/com/example/pasodedatos/NewUser.java
+++ app/src/main/java/com/example/pasodedatos/NewUser.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewUser extends AppCompatActivity {
 
     Button btnGuardar, btnCancelar;
+    EditText edtNombre, edtCorreo, edtPass, edtConfirPass;
+    RadioButton rdbUsuario, rdbAdmin, rdbAsistente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,13 +24,50 @@ public class NewUser extends AppCompatActivity {
         btnGuardar = findViewById(R.id.btnGuardar);
         btnCancelar = findViewById(R.id.btnCancelar);
 
+        edtNombre = findViewById(R.id.edtNombre);
+        edtCorreo = findViewById(R.id.edtCorreo);
+        edtPass = findViewById(R.id.edtPass);
+        edtConfirPass = findViewById(R.id.edtConfirPass);
+
+        rdbUsuario = findViewById(R.id.rdbUsuario);
+        rdbAdmin = findViewById(R.id.rdbAdmin);
+        rdbAsistente = findViewById(R.id.rdbAsistente);
+
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent objViewInfoUser = new Intent(getApplicationContext(), ViewNewUser.class);
-                objViewInfoUser.putExtra("btnCancelar", "Cancelar");
-                objViewInfoUser.putExtra("btnConfirmar", "Confirmar");
-                startActivityForResult(objViewInfoUser, 2);
+                String nombre = edtNombre.getText().toString().trim();
+                String correo = edtCorreo.getText().toString().trim();
+                String pass = edtPass.getText().toString().trim();
+                String confiPass = edtConfirPass.getText().toString().trim();
+                String tipoUsuario = rdbUsuario.isChecked() ? "Usuario" : rdbAdmin.isChecked() ? "Administrador" : "Asistente";
+
+                if (nombre.isEmpty()) {
+                    edtNombre.setError("El nombre es requerido");
+                    edtNombre.requestFocus();
+                } else if (correo.isEmpty()) {
+
+                    edtCorreo.setError("El correo es requerido");
+                    edtCorreo.requestFocus();
+                } else if (pass.equals(confiPass)) {
+
+                    Toast.makeText(getApplicationContext(), "Funciona", Toast.LENGTH_LONG).show();
+
+                    Intent objViewInfoUser = new Intent(getApplicationContext(), ViewNewUser.class);
+                    objViewInfoUser.putExtra("btnCancelar", "Cancelar");
+                    objViewInfoUser.putExtra("btnConfirmar", "Confirmar");
+                    objViewInfoUser.putExtra("nombre", nombre);
+                    objViewInfoUser.putExtra("correo", correo);
+                    objViewInfoUser.putExtra("pass", pass);
+                    objViewInfoUser.putExtra("tipoUsuario", tipoUsuario);
+                    startActivityForResult(objViewInfoUser, 2);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Tu contraseña no coincide", Toast.LENGTH_LONG).show();
+
+                }
+
+
 
             }
         });
--- app/src/main/java/com/example/pasodedatos/ViewNewUser.java
+++ app/src/main/java/com/example/pasodedatos/ViewNewUser.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ViewNewUser extends AppCompatActivity {
 
     Button btnConfirmar, btnCancelar;
+    TextView txtNombre, txtCorreo, txtClave, txtTipo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +20,41 @@ public class ViewNewUser extends AppCompatActivity {
 
         btnConfirmar = findViewById(R.id.btnConfirmar);
         btnCancelar = findViewById(R.id.btnCancelar);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtClave = findViewById(R.id.txtPass);
+        txtTipo = findViewById(R.id.txtTipo);
+
         Bundle datos = getIntent().getExtras();
+
         btnConfirmar.setText(datos.getString("btnConfirmar"));
         btnCancelar.setText(datos.getString("btnCancelar"));
+
+        String nombre = datos.getString("nombre");
+        String clave = datos.getString("pass");
+        String correo = datos.getString("correo");
+        String tipoUsuario = datos.getString("tipoUsuario");
+
+        if (nombre == null) {
+            nombre = "";
+        }
+
+        if (clave == null) {
+            clave = "";
+        }
+
+        if (correo == null) {
+            correo = "";
+        }
+        if (tipoUsuario == null) {
+            tipoUsuario = "";
+        }
+
+        txtNombre.setText("Nombre: " +nombre);
+        txtClave.setText("Clave: " +clave);
+        txtCorreo.setText("Correo: " +correo);
+        txtTipo.setText("Tipo de usuario: " +tipoUsuario);
 
         btnConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
