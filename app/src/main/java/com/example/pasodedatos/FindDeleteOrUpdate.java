8694e9600c7ca9ff114f105317a8d8723f971685
Kevin Mendez <kevin_mendez30@outlook.com>
2019-09-18 11:43:28 -0600

    Se agregan vistas a usar

---
+++ app/src/main/java/com/example/pasodedatos/FindDeleteOrUpdate.java
@@ -0,0 +1,14 @@
+package com.example.pasodedatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindDeleteOrUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_delete_or_update);
+    }
+}
---
+++ app/src/main/java/com/example/pasodedatos/ViewNewUser.java
@@ -0,0 +1,14 @@
+package com.example.pasodedatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_new_user);
+    }
+}
